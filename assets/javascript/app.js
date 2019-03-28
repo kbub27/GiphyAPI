49d12caeb58e1485f46ed1399929e3f89a980ce2
kbub27 <kalebmerriman27@gmail.com>
2019-03-27 18:29:52 -0700

    some javascript added along with api key variable

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    // GIPHY API URL LINK SAVED TO A VARIABLE
+    var giphyURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=M4QerXA1BTog7T6M4tVp4T87xtAk8rXK&limit=10";
+    // ARRAY OF STRINGS BASED ON THE THEME OF SPORTS
+    var sports = [];
+    // CREATE A FUNCTION TO LOOP THROUGH ARRAY AND CREATE A BUTTON FOR EACH ELEMENT IN THE ARRAY
+    function makeBtn() {
+        for (let i = 0; i < topics.length; i++) {
+            
+        }
+    }
+
+
+
+
+})
