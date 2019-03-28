d12ee1dd7d053439a66cf0cfe156cf1ead74c94a
kbub27 <kalebmerriman27@gmail.com>
2019-03-27 18:42:51 -0700

    make button function added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,15 +2,16 @@ $(document).ready(function () {
     // GIPHY API URL LINK SAVED TO A VARIABLE
     var giphyURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=M4QerXA1BTog7T6M4tVp4T87xtAk8rXK&limit=10";
     // ARRAY OF STRINGS BASED ON THE THEME OF SPORTS
-    var sports = [];
+    var sports = ['Basketball','Football','Soccer'];
     // CREATE A FUNCTION TO LOOP THROUGH ARRAY AND CREATE A BUTTON FOR EACH ELEMENT IN THE ARRAY
     function makeBtn() {
-        for (let i = 0; i < topics.length; i++) {
-            
+        $('.btns').empty();
+        for (let i = 0; i < sports.length; i++) {
+            $('.btns').append('<button class="btn btn-warning">' + sports[i] + '</button>' );
         }
     }
 
 
 
-
+    makeBtn();
 })
